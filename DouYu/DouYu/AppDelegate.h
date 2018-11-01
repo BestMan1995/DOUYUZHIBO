e937632e7d0c052b0b8b135f77befaf81d28492f
画个圈养唐僧 <1044901196@qq.com>
2018-11-01 17:58:46 +0800

    初始化项目

---
+++ DouYu/DouYu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DouYu
+//
+//  Created by Apple on 2018/11/1.
+//  Copyright © 2018年 wangBin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DouYu/DouYu/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DouYu
+//
+//  Created by Apple on 2018/11/1.
+//  Copyright © 2018年 wangBin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
